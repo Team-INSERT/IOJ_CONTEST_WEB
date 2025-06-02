3d8145e24cd4d0173921d8ded8dbfb257f760415
Jamkris <dltmdgus1412@gmail.com>
2025-06-02 17:53:48 +0900

    feat : Primary 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      olors: {
+        ut: {
+          white: '#FFF',
+          pink: '#FF48AB',
+          black: '#1C1C1C',
+          insertBlue: '#007CFF',
+          green: '#7ECE40',
+          warningRed: '#E33434',
+          correctGreen: '#24B984',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
