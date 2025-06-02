505b53d510b333bd972520686263b25b78828e97
Jamkris <dltmdgus1412@gmail.com>
2025-06-03 08:19:39 +0900

    feat : 문제 풀이 상태 컴포넌트 제작

---
+++ src/components/Contest/QuestionStatus.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface OwnProps {
+  status: 'unsolved' | 'solved' | 'failed';
+}
+
+const QuestionStatus = ({ status }: OwnProps) => {
+  return (
+    <div
+      className={`flex items-center justify-center w-8 rounded h-9 
+        ${
+          status === 'unsolved'
+            ? ''
+            : status === 'solved'
+              ? 'bg-[#24B984]'
+              : 'bg-[#E54747]'
+        }`}
+    >
+      <div className="text-white text-Ntext">
+        {status === 'unsolved' ? '' : status === 'solved' ? 'S' : 'W'}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionStatus;
