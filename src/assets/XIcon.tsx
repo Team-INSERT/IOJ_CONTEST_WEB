6df7682e7b725f9ea50e4ae72b86557bac112a0f
Jamkris <dltmdgus1412@gmail.com>
2025-06-04 09:49:31 +0900

    feat : X 아이콘 추가

---
+++ src/assets/XIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const XIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+    >
+      <circle cx="7" cy="7" r="7" fill="#E33434" />
+      <path
+        d="M10.0065 4L4 10M4 4L10.0065 10"
+        stroke="#811212"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default XIcon;
