bd8f2623da449dd7d7fd3a1e3c11d1d5e287774d
Jamkris <dltmdgus1412@gmail.com>
2025-06-04 19:36:48 +0900

    feat : Page404 제작

---
+++ src/app/not-found.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import InsertIcon from '@/assets/InsertIcon';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+const NotFound = () => {
+  const navigate = useRouter();
+
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen gap-12">
+      <div className="flex items-center justify-center gap-6">
+        <InsertIcon />
+
+        <div className="text-bt3 text-ut-insertBlue">Team Insert</div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center gap-5">
+        <h1 className="text-bt1 text-ut-warningRed">404 - Page Not Found</h1>
+
+        <div className="text-text">
+          쓸데없는 짓 하지 말고 문제 열심히 푸세용!!
+        </div>
+
+        <button
+          onClick={() => {
+            navigate.push('/');
+          }}
+          className="px-4 py-3 text-white rounded-md text-stext bg-ut-insertBlue"
+        >
+          돌아가기
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ src/assets/InsertIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const InsertIcon = () => {
+  return (
+    <svg
+      width="132"
+      height="121"
+      viewBox="0 0 192 181"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M59.1045 36.7471L191.998 0.855957V144.42L59.1045 180.312V36.7471Z"
+        fill="#0016B7"
+      />
+      <path
+        d="M0.348633 36.7471L133.243 0.855957V144.42L0.348633 180.312V36.7471Z"
+        fill="#2D47FF"
+      />
+    </svg>
+  );
+};
+
+export default InsertIcon;
