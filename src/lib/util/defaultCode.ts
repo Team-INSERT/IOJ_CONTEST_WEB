6bdd56a121fde99ab455864a6ee270b215eaa151
Jamkris <dltmdgus1412@gmail.com>
2025-06-09 10:54:20 +0900

    chore : 기본 로직 파일 분리

---
+++ src/lib/util/defaultCode.ts
@@ -0,0 +1,8 @@
+const defaultCode = {
+  c: `#include <stdio.h>\n\nint main() {\n  int a, b;\n  scanf("%d %d", &a, &b);\n  printf("%d\\n", a * b);\n  return 0;\n}`,
+  c_cpp: `#include <iostream>\n\nusing namespace std;\n\nint main() {\n  int a, b;\n  cin >> a >> b;\n  cout << a * b << endl;\n  return 0;\n}`,
+  java: `import java.util.*;\n\npublic class Main {\n  public static void main(String[] args) {\n    Scanner sc = new Scanner(System.in);\n    int a = sc.nextInt();\n    int b = sc.nextInt();\n    System.out.println(a * b);\n  }\n}`,
+  python: `a, b = map(int, input().split())\nprint(a * b)`,
+};
+
+export default defaultCode;
--- src/lib/util/index.ts
+++ src/lib/util/index.ts
@@ -2,3 +2,4 @@ export { default as Storage } from './storge';
 export { default as FormatUtil } from './formatting';
 export { default as PathUtil } from './path';
 export { default as AlertUtil } from './alert';
+export { default as defaultCode } from './defaultCode';
