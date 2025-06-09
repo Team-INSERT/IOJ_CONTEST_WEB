a6ebe80d48c4c688dab01fa6519ec0e45f9e2ff7
Jamkris <dltmdgus1412@gmail.com>
2025-06-09 09:59:26 +0900

    feat : Loading 컴포넌트 제작

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen">
+      <div className="text-bt3">Loading...</div>
+    </div>
+  );
+};
+
+export default Loading;
