6be707d92cf741720fb64d063951bd62a919d952
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-06-25 15:08:47 +0900

    style : Login 페이지 디자인 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import GoogleIcon from '@/assets/GoogleIcon';
+import Logo from '@/assets/Logo';
 import { useGetLoginLink } from '@/lib/service/auth/auth.query';
 import React from 'react';
 
@@ -17,17 +18,23 @@ const Login = () => {
   }
 
   return (
-    <div className="flex items-center justify-center w-full h-full bg-gray-800 cursor-pointer">
+    <div className="flex flex-col items-center w-full h-full gap-[8.25rem]">
+      <header className="flex flex-col items-center pt-56 gap-11">
+        <Logo />
+        <p className="text-[1.1875rem] text-gray-800 font-regular">
+          아이템을 이용한 스릴 넘치는 코딩 경기, IOJ에 오신 걸 환영합니다!
+        </p>
+      </header>
       <div
-        className="flex items-center justify-center bg-white border border-gray-200 w-fit h-fit py-[0.69rem] px-[6.56rem] rounded relative shadow-defaultShadow"
+        className="flex items-center justify-center bg-white border border-gray-200 h-fit py-[11px] px-[124.5px] cursor-pointer rounded-lg gap-[0.6875rem]"
         onClick={() => {
           window.location.href = data;
         }}
       >
-        <div className="absolute left-3">
-          <GoogleIcon />
+        <GoogleIcon />
+        <div className="text-[16px] text-[#353535] font-regular">
+          구글 로그인
         </div>
-        <div className="text-stext">구글 로그인</div>
       </div>
     </div>
   );
