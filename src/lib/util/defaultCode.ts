71bff8f3f46edb43169416196c0f071a1aa77b75
Jamkris <dltmdgus1412@gmail.com>
2025-06-27 08:19:53 +0900

    fix : java 디폴트코드 수정

--- src/lib/util/defaultCode.ts
+++ src/lib/util/defaultCode.ts
@@ -1,7 +1,7 @@
 const defaultCode = {
   C: `#include <stdio.h>\n\nint main() {\n  int a, b;\n  scanf("%d %d", &a, &b);\n  printf("%d\\n", a * b);\n  return 0;\n}`,
   CPP: `#include <iostream>\n\nusing namespace std;\n\nint main() {\n  int a, b;\n  cin >> a >> b;\n  cout << a * b << endl;\n  return 0;\n}`,
-  JAVA: `import java.util.*;\n\npublic class Main {\n  public static void main(String[] args) {\n    Scanner sc = new Scanner(System.in);\n    int a = sc.nextInt();\n    int b = sc.nextInt();\n    System.out.println(a * b);\n  }\n}`,
+  JAVA: `import java.util.*;\n\npublic class main {\n  public static void main(String[] args) {\n    Scanner sc = new Scanner(System.in);\n    int a = sc.nextInt();\n    int b = sc.nextInt();\n    System.out.println(a * b);\n  }\n}`,
   PYTHON: `a, b = map(int, input().split())\nprint(a * b)`,
 };
 
